61e81f5fa321898cf25447f4a80378144d984969
Nasimjon <smartboy9250000@gmail.com>
2022-07-27 22:01:04 +0500

    460px ham bitti

--- js.js
+++ js.js
@@ -112,3 +112,56 @@ else if(screen.width >= 798 && screen.width <= 999){
         },900)
     })
 }
+
+else if(screen.width <= 460){
+    btn1.addEventListener("click", () =>{
+        setTimeout(function(){
+            images[0].style.opacity = "0"
+            images[1].style.opacity = "0"
+            images[2].style.opacity = "0"
+        },300) 
+        setTimeout(function(){
+            images[0].style.transform = "translateX(180px)"
+            images[1].style.transform = "translateX(180px)"
+            images[2].style.transform = "translateX(180px)"
+    
+        },600)
+        setTimeout(()=>{
+            images[0].style.opacity = "1"
+        },900)
+    })
+    
+    btn2.addEventListener("click", () =>{
+        setTimeout(function(){
+            images[0].style.opacity = "0"
+            images[1].style.opacity = "0"
+            images[2].style.opacity = "0"
+        },300) 
+        setTimeout(function(){
+            images[0].style.transform = "translateX(0px)"
+            images[1].style.transform = "translateX(0px)"
+            images[2].style.transform = "translateX(0px)"
+    
+        },600)
+        setTimeout(()=>{
+            images[1].style.opacity = "1"
+        },900)
+    })
+    btn3.addEventListener("click", () =>{
+        setTimeout(function(){
+            images[0].style.opacity = "0"
+            images[1].style.opacity = "0"
+            images[2].style.opacity = "0"
+        },300) 
+        setTimeout(function(){
+            images[0].style.transform = "translateX(-180px)"
+            images[1].style.transform = "translateX(-180px)"
+            images[2].style.transform = "translateX(-180px)"
+    
+    
+        },600)
+        setTimeout(()=>{
+            images[2].style.opacity = "1"
+        },900)
+    })
+}
